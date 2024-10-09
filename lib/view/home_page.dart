eee3e5e47602ac9fc0bc5d7319cf2354366d62e5
denz9993j <denz9993j@gmail.com>
2024-10-09 17:57:34 +0000

    homepage const

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/controller/calculator_controller.dart';
+import 'package:myapp/model/calculator_model.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -27,11 +30,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Expanded(
       child: OutlinedButton(
         style: OutlinedButton.styleFrom(
-          padding: EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(24.0),
         ),
         child: Text(
           buttonText,
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
         onPressed: () => controller.buttonPressed(buttonText, updateView),
       ),
@@ -44,52 +47,50 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
-              child: Text(
-                controller.output,
-                style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
-              ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              controller.output,
+              style: const TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
             ),
-            Expanded(child: Divider()),
-            Column(
-              children: [
-                Row(children: [
-                  buildButton("7"),
-                  buildButton("8"),
-                  buildButton("9"),
-                  buildButton("/")
-                ]),
-                Row(children: [
-                  buildButton("4"),
-                  buildButton("5"),
-                  buildButton("6"),
-                  buildButton("X")
-                ]),
-                Row(children: [
-                  buildButton("1"),
-                  buildButton("2"),
-                  buildButton("3"),
-                  buildButton("-")
-                ]),
-                Row(children: [
-                  buildButton("."),
-                  buildButton("0"),
-                  buildButton("00"),
-                  buildButton("+")
-                ]),
-                Row(children: [
-                  buildButton("CLEAR"),
-                  buildButton("="),
-                ])
-              ],
-            )
-          ],
-        ),
+          ),
+          const Expanded(child: Divider()),
+          Column(
+            children: [
+              Row(children: [
+                buildButton("7"),
+                buildButton("8"),
+                buildButton("9"),
+                buildButton("/")
+              ]),
+              Row(children: [
+                buildButton("4"),
+                buildButton("5"),
+                buildButton("6"),
+                buildButton("X")
+              ]),
+              Row(children: [
+                buildButton("1"),
+                buildButton("2"),
+                buildButton("3"),
+                buildButton("-")
+              ]),
+              Row(children: [
+                buildButton("."),
+                buildButton("0"),
+                buildButton("00"),
+                buildButton("+")
+              ]),
+              Row(children: [
+                buildButton("CLEAR"),
+                buildButton("="),
+              ])
+            ],
+          )
+        ],
       ),
     );
   }
